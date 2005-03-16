cf9f1709b7c5ff4fb2488281118c721c5fda9990
Arild <Arild@23913781-f3ab-ca43-b79f-3fa6c8d344c4>
2005-03-16 02:24:33 +0000

    Added a build.bat
    git-svn-id: svn://localhost@50 23913781-f3ab-ca43-b79f-3fa6c8d344c4

---
+++ trunk/build.bat
@@ -0,0 +1,26 @@
+set targdir=%1
+
+rmdir /s /q \tmp\thesis
+mkdir \tmp\thesis
+svn checkout http://10.0.0.3/svn/masters/trunk/thesis \tmp\thesis\thesis
+svn checkout http://10.0.0.3/svn/masters/trunk/tools \tmp\thesis\tools
+
+pushd
+
+cd \tmp\thesis\tools\Transform
+make
+
+cd ..\..\thesis\
+
+make html
+cmd /c make pdf
+
+mkdir %targdir%
+copy /y html\thesis.html %targdir%
+copy /y pdf\thesis.pdf %targdir%
+
+cd ..
+
+copy /y tools\styles.css %targdir%
+
+popd
